f79e8a3874bdb7b4ac96b73a5f3e99140ef5bc9f
astable <astable@Santis-Mac-mini.local>
2021-02-18 18:54:50 +0100

    Nueva funcionalidad

--- src/main/java/com/sanvalero/miaplicacion/Application.java
+++ src/main/java/com/sanvalero/miaplicacion/Application.java
@@ -3,5 +3,6 @@ package com.sanvalero.miaplicacion;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Esta es mi aplicación");
+        System.out.println("Nueva funcionalidad");
     }
 }
